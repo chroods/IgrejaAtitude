45a38c133966aa75a4b8b0928d0e205c21d09e23
David Wanderon <daviddonnna@hotmail.com>
2023-03-25 20:40:16 -0300

    modified:   src/components/Menu/index.tsx 	modified:   src/components/MenuLeft/index.tsx 	modified:   src/components/MenuLeft/styles.ts

--- src/components/MenuLeft/index.tsx
+++ src/components/MenuLeft/index.tsx
@@ -1,39 +1,51 @@
 import React from "react";
-import { Container } from "./styles";
+import {BsYoutube} from 'react-icons/bs'
+import {AiFillInstagram} from 'react-icons/ai'
+import { 
+    Container,
+    RedesMenu
+ } from "./styles";
 
 interface Props{
-    clicado: boolean
+    clicado: boolean;
 }
 
 export function MenuLeft({clicado}: Props){
+    const clasMenu = clicado != false ? 'menuactive' : 'menudisabled';
+
     return(
         <Container clicado={clicado}>
             <ul>
-                <li>
+                <li className={clasMenu}>
                     <a href="#">Sobre nòs</a>
                 </li>
-                <li>
+                <li className={clasMenu}>
                     <a href="#">Embaixadas</a>
                 </li>
-                <li>
+                <li className={clasMenu}>
                     <a href="#">Agenda</a>
                 </li>
-                <li>
+                <li className={clasMenu}>
                     <a href="#">Cursos</a>
                 </li>
-                <li>
+                <li className={clasMenu}>
                     <a href="#">Eventos</a>
                 </li>
-                <li>
+                <li className={clasMenu}>
                     <a href="#">Video</a>
                 </li>
-                <li>
+                <li className={clasMenu}>
                     <a href="#">Pastores</a>
                 </li>
-                <li>
+                <li className={clasMenu}>
                     <a href="#">Contato</a>
                 </li>
             </ul>
+
+            <RedesMenu>
+                <BsYoutube className="BsYoutube"/>
+                <AiFillInstagram className="AiFillInstagram" />
+            </RedesMenu>
         </Container>
     )
 }
--- src/components/MenuLeft/styles.ts
+++ src/components/MenuLeft/styles.ts
@@ -18,19 +18,69 @@ export const Container = styled.div<Props>`
     z-index: 2;
     border-radius: 0 20px 20px 0;
     transition: 600ms all ease-in-out;
+    background: rgb(0,143,255);
+    background: linear-gradient(140deg, rgba(0,143,255,1) 0%, rgba(2,36,117,1) 67%, rgba(3,14,89,1) 100%);
+
+    @keyframes animateMenu {
+        0%{
+            transform: translateX(-100px);
+        }
+        50%{
+            transform: translateX(50px);
+        }
+        100%{
+            transform: translateX(0);
+        }
+    }
+
+    .menuactive{
+        animation: animateMenu 1s both;
+    }
 
     ul{
+        width: 100%;
         display: flex;
         flex-direction: column;
-        gap: 20px;
+        gap: 10px;
     }
 
     li a{
+        display: block;
+        width: 100%;
+        padding: 10px;
+        transition: 1s all ease-in-out;
+        text-align: center;
         font-size: 20px;
         color: #dedede;
+    }
+
+    li a:hover{
+        position: relative;
+        transition: 1s all ease-in-out;
+        background-color: #ffff;
+        color: #000;
     }
 
     @media (min-width: 1024px){
         transition: 600ms all ease-in-out;
     }
 `
+
+export const RedesMenu = styled.div`
+    position: absolute;
+    bottom: 15px;
+    display: flex;
+    gap: 20px;
+    font-size: 2rem;
+    color: #fff;
+
+    .BsYoutube:hover{
+        color: red;
+        cursor: pointer;
+    }
+
+    .AiFillInstagram:hover{
+        color: #E600C2;
+        cursor: pointer;
+    }
+`;
