240c178138961978a339d013e0bf88404ec9c8f5
David Wanderson <daviddonnna@hotmail.com>
2023-04-02 10:15:45 -0300

    modified:   src/components/Menu/index.tsx 	modified:   src/components/Menu/styles.ts

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -7,27 +7,35 @@ export function Menu(){
             <ul>
                 <li>
                     <a href="#">Sobre nòs</a>
+                    <div className="barraHover"></div>
                 </li>
                 <li>
                     <a href="#">Embaixadas</a>
+                    <div className="barraHover"></div>
                 </li>
                 <li>
                     <a href="#">Agenda</a>
+                    <div className="barraHover"></div>
                 </li>
                 <li>
                     <a href="#">Cursos</a>
+                    <div className="barraHover"></div>
                 </li>
                 <li>
                     <a href="#">Eventos</a>
+                    <div className="barraHover"></div>
                 </li>
                 <li>
                     <a href="#">Video</a>
+                    <div className="barraHover"></div>
                 </li>
                 <li>
                     <a href="#">Pastores</a>
+                    <div className="barraHover"></div>
                 </li>
                 <li>
                     <a href="#">Contato</a>
+                    <div className="barraHover"></div>
                 </li>
             </ul>
         </Container>
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -14,10 +14,47 @@ export const Container = styled.div`
         font-weight: 400;
     }
 
+    li:hover .barraHover{
+        background-color: #022155;
+        animation: barraHover 1s ease-in-out both;
+        transition: 600ms all ease-in-out;
+    }
+
     li a{color:#222}
     li a:hover{color:#000}
 
+    @keyframes barraHover{
+        0%{
+            width: 0;
+        }
+
+        100%{
+            width: 100%;
+        }
+    }
+
+    @keyframes barraHoverEnd{
+        0%{
+            width: 100%;
+        }
+
+        100%{
+            width: 0;
+        }
+    }
+
+    .barraHover{
+        position: relative;
+        bottom: 0;
+        height: 2px;
+        width: 100%;
+        z-index: 1;
+        animation: barraHoverEnd 1s ease-in-out both;
+        transition: 600ms all ease-in-out;
+    }
+
     @media (max-width: 1024px){
         display: none;
+        transition: 600ms all ease-in-out;
     }
 `;
